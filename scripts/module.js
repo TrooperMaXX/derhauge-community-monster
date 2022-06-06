a15890f3d3b3cde8570fbead986608fbc4f255a3
TrooperMaXX <trooper@troopermaxx.de>
2022-06-07 01:15:38 +0200

    - init commit - added Clockwork Destructor Mk3 by Figgu#8859

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,7 @@
 Hooks.once('init', async function() {
-
+    console.log('hauge-community-monster | init');
 });
 
 Hooks.once('ready', async function() {
-
+    console.log('hauge-community-monster | Hey Na?!');
 });
